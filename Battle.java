08ca3273c6498800f7b174e9f5a586d3017b4b74
Pacheca <mpacheca7@gmail.com>
2025-04-07 17:53:31 +0200

    codigo actualizado

--- Battle.java
+++ Battle.java
@@ -12,20 +12,6 @@ public class Battle {
 
         while(true){
             System.out.println("Round: " + round);
-
-            int countDamage1 = IAttacker.damage1; // Usamos el ataque aleatorio de la interfaz
-            int countDamage2 = IAttacker.damage2;
-
-            System.out.println(p1.getName() + " attacks, and does " + countDamage1 + " of damage"); // nos dice cuanto ataque tiene
-            System.out.println(p2.getName() + " attacks, and does " + countDamage2 + " of damage");
-
-            p1.setHp(p1.getHp() - countDamage1); // Resta el ataque - vida
-            p2.setHp(p2.getHp() - countDamage2);
-
-            System.out.println(p1.getName() + " has " + p1.getHp() + " of HP");
-            System.out.println(p2.getName() + " has " + p2.getHp() + " of HP");
-            break;
-
         }
 
     }
