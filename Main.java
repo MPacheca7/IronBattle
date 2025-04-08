8a0d20859f61b0518fb554e574b668bffe748a83
Irenevn16 <irenevn16@gmail.com>
2025-04-08 12:16:11 +0200

    main

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // Players
-        //Character player1 = new Warrior("Aragorn");
-        //Character player2 = new Wizard("Gandalf");
+        Warrior player1 = new Warrior("Aragorn");
+        Wizard player2 = new Wizard("Gandalf");
 
+        System.out.println(player1.getName() + " vs " + player2.getName());
+        System.out.println("The Warrior " + player1.getName() + " is " + player1.getHp() + "HP, " +  player1.getStamina() + " of Stamina, and " + player1.getStrength() + " Strength");
+        System.out.println("The Wizard " + player2.getName() + " is " + player2.getHp() + "HP, " +  player2.getMana() + " of Mana, and " + player2.getIntelligence() + " Intelligence");
         //Battle
-        //Battle battle = new Battle(player1, player2);
+        Battle battle = new Battle(player1, player2);
 
-        //battle.StartingBattle(); // Llama a la ronda 1
+        battle.StartingBattle(); // Llama a la ronda 1
     }
 }
